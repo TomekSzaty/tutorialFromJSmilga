0dcaf79e59c0326eafaf4a5ddb60788df3ed8d05
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2023-04-10 16:57:03 +0200

    Added settings extensions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return <h2>My first component.</h2>
+    return <h2 id="someId" className="myClass">Hi there !!</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
